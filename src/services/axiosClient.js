caa03881824f9d2692d81f8b36fb7c1975ba9276
Sara Ahmad <111248837+sara8ahmad@users.noreply.github.com>
2024-05-30 21:50:40 +0300

    Update axiosClient.js

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -1,6 +1,6 @@
 const {default : axios} = require('axios');
 const api_key = process.env.NEXT_PUBLIC_REST_API_KEY;
-const api_url = 'http://localhost:1337/api';
+const api_url = 'https://strapi-backend-h32u.onrender.com/admin/content-manager/collection-types/api';
 
 
 const axiosClient = axios.create({
